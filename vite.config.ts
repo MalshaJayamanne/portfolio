30461fcd9371b98125399aaf2db49069d466a4f6
Malsha Jayamanne <malshasulakshan4@gmail.com>
2026-07-03 00:16:12 +0530

    final

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,15 @@
 import { defineConfig } from 'vite'
-import { tanstackStart } from '@tanstack/react-start/plugin/vite'
 import tailwindcss from '@tailwindcss/vite'
 import react from '@vitejs/plugin-react'
 import path from 'path'
+import { TanStackRouterVite } from '@tanstack/router-plugin/vite'
 
 export default defineConfig({
-  plugins: [tanstackStart(), tailwindcss(), react()],
+  plugins: [
+    TanStackRouterVite(),
+    tailwindcss(),
+    react(),
+  ],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
